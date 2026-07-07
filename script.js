5569e48233d078a1b2c118f5009e9bf3e48298a0
Toby Hutchinson <tobyhutchinson@gmail.com>
2026-07-07 22:20:11 +0100

    Added text in initial position

--- script.js
+++ script.js
@@ -16,6 +16,85 @@ let ball = null
 
 const scoreEl = document.getElementById('score');
 
+// === Create the text ===
+const nameEl = document.getElementById("name");
+const aboutEl = document.getElementById("about");
+const linkEls = document.getElementsByClassName("contact-link");
+
+function getCssStyle(element, prop) {
+    return window.getComputedStyle(element, null).getPropertyValue(prop);
+}
+
+function getCanvasFont(el) {
+  const fontWeight = getCssStyle(el, 'font-weight') || 'normal';
+  const fontSize = getCssStyle(el, 'font-size') || '16px';
+  const fontFamily = getCssStyle(el, 'font-family') || 'Times New Roman';
+  
+  return `${fontWeight} ${fontSize} ${fontFamily}`;
+}
+
+// Store position and value data
+let textElements = {
+    "elements": [
+        {
+            "value": nameEl.innerText,
+            "position": {
+                "top": nameEl.getBoundingClientRect().top,
+                "left": nameEl.getBoundingClientRect().left,
+                "width": nameEl.getBoundingClientRect().width
+            },
+            "styles": {
+                "font": getCanvasFont(nameEl),
+                "fontsize": getCssStyle(nameEl, "font-size")
+            }
+        },
+        {
+            "value": aboutEl.innerText,
+            "position": {
+                "top": aboutEl.getBoundingClientRect().top,
+                "left": aboutEl.getBoundingClientRect().left,
+                "width": aboutEl.getBoundingClientRect().width
+            },
+            "styles": {
+                "font": getCanvasFont(aboutEl),
+                "fontsize": getCssStyle(aboutEl, "font-size")
+            }
+        }
+    ]
+};
+
+function getCharWidth(char, font) {
+    const canvas = getCharWidth.canvas || (getCharWidth.canvas = document.createElement("canvas"));
+    const context = canvas.getContext("2d");
+    context.font = font;
+    return context.measureText(char).width;
+}
+
+
+nameEl.style.display = "none";
+aboutEl.style.display = "none";
+for (let el of linkEls) {
+    el.style.display = "none";
+}
+
+// Display the text in initial positions
+// We actually need to store positions of each character 
+const el = textElements.elements[0];
+let textLeft = el.position.left;
+let textTop = el.position.top;
+for (let char of textElements.elements[0].value) {
+    let span = document.createElement("span");
+    const charWidth = getCharWidth(char, el.styles.font);
+    span.style.fontSize = el.styles.fontsize;
+    span.style.font = el.styles.font;
+    span.textContent = char;
+    span.style.position = "absolute";
+    span.style.left = textLeft + "px";
+    span.style.top = textTop + "px";
+    document.body.appendChild(span);
+    textLeft += charWidth * 1.01; 
+}
+
 // Create the ball in the DOM
 const ballRadius = 50;
 const ballEl = document.createElement("div")
